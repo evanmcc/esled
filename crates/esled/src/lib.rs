1d0033e65bc52aef8e3e03ce1f60bbe89bc41a5c
Evan Vigil-McClanahan <evan@helium.com>
2019-05-08 19:14:23 -0700

    progress

--- crates/esled/src/lib.rs
+++ crates/esled/src/lib.rs
@@ -1,23 +1,23 @@
-#[macro_use]
+//#[macro_use]
 extern crate erlang_nif_sys;
 extern crate sled;
 
 use erlang_nif_sys::*;
 use sled::Db;
 
+use std::{mem, ptr, path::Path, str, slice};
+//use std::cmp::min;
+use std::sync::atomic::{AtomicIsize, Ordering};
 
 static mut SLEDDB_TYPE: *const ErlNifResourceType = 0 as *const ErlNifResourceType;
 static mut DTOR_COUNTER: Option<AtomicIsize> = None;
 
 nif_init!("mynifmod",
           [
-              ("times2", 1, slice_args!(times2)),
-              ("test_enif_make_pid", 0, test_enif_make_pid),
-              ("rustmap", 0, rustmap),
-              ("rustmap_dtor_count", 0, rustmap_dtor_count),
-              ("to_str", 1, slice_args!(to_str)),
-              ("hash", 1, slice_args!(hash)),
-              ("make_map", 0, slice_args!(make_map)),
+              ("open", 1, slice_args!(open)),
+              ("put", 3, slice_args!(put)),
+              ("get", 2, slice_args!(get)),
+              ("sleddb_dtor_count", 0, sleddb_dtor_count)
           ],
           {load: esled_load});
 
@@ -36,22 +36,70 @@ unsafe fn esled_load(env: *mut ErlNifEnv,
     SLEDDB_TYPE.is_null() as c_int
 }
 
-fn times2(env: *mut ErlNifEnv, args: &[ERL_NIF_TERM]) -> ERL_NIF_TERM {
+unsafe extern "C" fn sleddb_destructor(_env: *mut ErlNifEnv, handle: *mut c_void) {
+    DTOR_COUNTER.as_mut().unwrap().fetch_add(1, Ordering::SeqCst);
+    let db = ptr::read(handle as *mut Db);
+    db.flush();
+}
+
+fn open(env: *mut ErlNifEnv, args: &[ERL_NIF_TERM]) -> ERL_NIF_TERM {
+    let db = match args.len() {
+        1 => {
+            let path = bin_to_slice(env, args[0]);
+            let path = str::from_utf8(path).unwrap();
+            match Db::start_default(Path::new(path)) {
+                Ok(db) => {
+                    db
+                }
+                Err(_) => { // improve this
+                    return unsafe { enif_make_badarg(env) }
+                }
+            }
+        }
+        _ => {
+            return unsafe { enif_make_badarg(env) }
+        }
+    };
     unsafe {
-        let mut result: i32 = mem::uninitialized();
-        if 1==args.len() && 0!=enif_get_int(env, args[0], &mut result) {
-            enif_make_int(env, 2*result)
-        }
-        else {
-            enif_make_badarg(env)
-        }
+        let mem = enif_alloc_resource(SLEDDB_TYPE, mem::size_of::<Db>());
+        assert_eq!(mem as usize % mem::align_of::<Db>(), 0);
+        ptr::write(mem as *mut Db, db);
+        let term = enif_make_resource(env, mem);
+        enif_release_resource(mem);
+        term
     }
 }
 
-fn test_enif_make_pid(env: *mut ErlNifEnv, _: c_int, _: *const ERL_NIF_TERM) -> ERL_NIF_TERM {
-    let mut pid: ErlNifPid = unsafe { mem::uninitialized() };
-    unsafe { enif_self(env, &mut pid) };
-    unsafe { enif_make_pid(env, &pid) }
+fn put(env: *mut ErlNifEnv, args: &[ERL_NIF_TERM]) -> ERL_NIF_TERM {
+    let (db, key, value) = match args.len() {
+        3 => {
+            let d: &Db = mem::unintialized();
+            let d = unsafe { enif_get_resource(env, args[0], SLEDDB_TYPE, &d) };
+            let k = bin_to_slice(env, args[1]);
+            let v = bin_to_slice(env, args[2]);
+            (d, k, v)
+        }
+        _ => {
+            return unsafe { enif_make_badarg(env) }
+        }
+    }
+    match db.set(key, value) {
+        Ok(_) =>
+            return atom ok
 }
 
+fn get(env: *mut ErlNifEnv, args: &[ERL_NIF_TERM]) -> ERL_NIF_TERM {
+}
 
+unsafe fn sleddb_dtor_count(env: *mut ErlNifEnv, _: c_int, _: *const ERL_NIF_TERM) -> ERL_NIF_TERM {
+    let cnt = DTOR_COUNTER.as_mut().unwrap().load(Ordering::SeqCst);
+    enif_make_int(env, cnt as i32)
+}
+
+fn bin_to_slice<'a>(env: *mut ErlNifEnv, term: ERL_NIF_TERM) -> &'a [u8] {
+    unsafe {
+        let mut bin: ErlNifBinary = mem::uninitialized();
+        enif_inspect_binary(env, term, &mut bin);
+        slice::from_raw_parts(bin.data, bin.size)
+    }
+}
